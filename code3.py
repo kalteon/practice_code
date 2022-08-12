88adee1bdbe9102888847d105f8eb6dc30a1b60c
kalteon <tnghdjaak75@gmail.com>
2022-08-12 15:07:21 +0900

    Initial commit

---
+++ code3.py
@@ -0,0 +1,13 @@
+def reorderLogFiles(logs) -> list[str]:
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    return letters + digits
+
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+print(reorderLogFiles(logs))
