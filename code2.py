ac0da065c87f2d67fa937d9ac8fa540f5f31dc13
kalteon <tnghdjaak75@gmail.com>
2022-08-12 14:52:33 +0900

    Initial commit

--- code2.py
+++ code2.py
@@ -1,10 +1,14 @@
-def reverseString(s) -> None:
+def reverseString1(s) -> None:
     left, right = 0, len(s) -1
     while left < right:
         s[left], s[right] = s[right], s[left]
         left += 1
         right -= 1
+def reverseString2(s) -> None:
+    s.reverse
 
 case = ["h", "e", "l", "l", "o"]
-reverseString(case)
+reverseString1(case)
+print(case)
+reverseString2(case)
 print(case)
