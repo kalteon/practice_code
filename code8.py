b897eed3ada33dc0fbb14da88f269bf725644e49
kalteon <tnghdjaak75@gmail.com>
2022-08-17 15:19:18 +0900

    Initial commit

---
+++ code8.py
@@ -0,0 +1,16 @@
+def trap(height: list[int]) -> int:
+    stack = []
+    volume = 0
+
+    for i in range(len(height)):
+        while stack and height[i] > height[stack[-1]]:
+            top = stack.pop()
+            if not stack:
+                break
+            distance = i - stack[-1] - 1
+            waters = min(height[i], height[stack[-1]]) - height[top]
+            volume += distance * waters
+
+        stack.append(i)
+    return volume
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
