678f5d03891690edab18c8f6b049ed33843aad89
kalteon <tnghdjaak75@gmail.com>
2022-08-23 15:08:51 +0900

    change deq to runner

--- code13.py
+++ code13.py
@@ -5,7 +5,7 @@ class Node:
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
-def isPalindrome(head) -> bool:
+def isPalindrome1(head) -> bool:
     q = collections.deque()
     if not head:
         return True
@@ -17,9 +17,28 @@ def isPalindrome(head) -> bool:
         if q.popleft() != q.pop():
             return False
     return True
+def isPalindrome2(head) -> bool:
+    slow = fast = head
+    revers = None
+    lead = head
+    middle = None
+    while fast and fast.next:
+        fast = fast.next.next
+        revers, revers.next, slow = slow, revers, slow.next
+        trail = middle
+        middle = lead
+        lead = lead.next
+        middle.next = trail
+    if fast:
+        slow = slow.next
+    while revers and revers.data == slow.data:
+        slow = slow.next
+        revers = revers.next
+    return not revers
+
 node1 = Node(1)
-print(isPalindrome(node1))
+print(isPalindrome1(node1))
 node1.next = Node(2)
-print(isPalindrome(node1))
+print(isPalindrome1(node1))
 node1.next = Node(1)
-print(isPalindrome(node1))
+print(isPalindrome2(node1))
