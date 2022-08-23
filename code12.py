c57b138db1ecfdef96492798f83c6372d7d5e279
kalteon <tnghdjaak75@gmail.com>
2022-08-23 14:06:16 +0900

    Initial commit

--- code12.py
+++ code12.py
@@ -5,4 +5,13 @@ def maxProfit(prices: list[int]) -> int:
         min_value = min(min_value, price)
         profit = max(profit, price - min_value)
     return profit
+def max_profit(prices: list[int]) -> int:
+    profit = 0
+    before_price = prices[0]
+    for price in prices[1:]:
+        if before_price < price:
+            profit += price - before_price
+        before_price = price
+    return profit
 print(maxProfit([7,1,5,3,6,4]))
+print(max_profit([7,1,5,3,6,4]))
